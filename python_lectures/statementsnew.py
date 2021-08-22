a1bdaa82ebf5531fc67ec9aa2f0068364ecbbf2f
Drakareous <kamblea122@gmail.com>
2021-08-22 14:30:18 +0530

    first comment

---
+++ python_lectures/statementsnew.py
@@ -0,0 +1,67 @@
+# #if statements 
+# if 1==1:
+#     print('im in if statement')
+# else:
+#     print('im in else statement')  
+# if 2>3:
+#     print('it is true')
+# else:
+#     print('it is false')
+
+# if ('true'):
+#     print('1==1')
+# else:
+#     print('1<1')                 
+
+# if False:
+#     print('a==a')
+# else:
+#     print('a<b')      
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+# a=int(input('enter value of a'))
+# b=int(input('enter value of b'))
+# if a>b:
+#     print('a is smaller than b')
+#     if (a-b >= 2):
+#         print('a is greater than b{}'.format(a-b))
+# elif a<b:
+#     print('a is greater than b')
+# else:
+#     print('a is equal to b')        
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+
+# list2=[1,2,3,4,5,6]
+# print(list2)
+# for item in list2 :
+#     if not(item % 2):
+#        print(item)
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+
+# list1=[1,2,3,4,5]
+# print(list1)
+# for item in list1 :    
+#     if not (item % 2):
+#            print(item)
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+
+#for loop with sorting
+# d={'a':'akshay','b':'banana','c':'car'}
+# for w,e in d.items():
+#     print(w)
+#     print('value is',e)
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+c=('akshay','rohit','shivam','akash')
+for q in .sort():
+    print(q)
+
+
+
+    
+     
+     
