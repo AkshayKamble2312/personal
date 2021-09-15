b4a5367a631a553938ae404d7d6c9b2bf2ab2310
Drakareous <kamblea122@gmail.com>
2021-09-15 16:19:13 +0530

    'add'

--- akshay/python_lectures/daily.py
+++ akshay/python_lectures/daily.py
@@ -23,7 +23,39 @@
 # text=f.read()
 # print(text)
 # f.close()
-#---------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------
+
+
+# chapter 1
+
+# practice session from code with harry
+
+# print('''Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.''')
+
+#---------------------------------------------------------------------------------------------------------------
+
+#using module from external source
+
+# from playsound import playsound
+
+# playsound("Downloads\\\\play.mp3")
+
+#---------------------------------------------------------------------------------------------------------------------
+
+# write a program to print the content
+
+# import os
+# print(os.listdir())
+
+#---------------------------------------------------------------------------------------------------------------------------
+# chapter 2 
+# veriable and datatypes
+  
+
+
 
 
 
